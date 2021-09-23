77fa2621df567ce10e288421a9f2e9f6ddedcbc1
Long <pagelayout8@gmail.com>
2021-09-23 17:38:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hom nay demo.
+          Edit <code>src/App.js</code> Long dep trai.
         </p>
         <a
           className="App-link"
